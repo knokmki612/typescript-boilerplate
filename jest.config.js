cc5835704ed676aa964eaf171e59f6ce96c1f54d
Kimiaki Kuno <knokmki612@gmail.com>
2022-04-02 12:08:54 +0900

    chore(jest): use ts-jest preset & default config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  roots: ["<rootDir>"],
-  testMatch: [
-    "**__tests__**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)",
-  ],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
+  preset: "ts-jest"
 };
